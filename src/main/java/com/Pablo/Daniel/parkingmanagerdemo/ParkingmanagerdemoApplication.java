d1081ef8f7c31e829feadeaaf46010e9b0115e0c
danigm11 <danigm821@gmail.com>
2023-01-18 19:59:46 +0100

    inicio

---
+++ src/main/java/com/Pablo/Daniel/parkingmanagerdemo/ParkingmanagerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.Pablo.Daniel.parkingmanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingmanagerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingmanagerdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pablo/Daniel/parkingmanagerdemo/ParkingmanagerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pablo.Daniel.parkingmanagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingmanagerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

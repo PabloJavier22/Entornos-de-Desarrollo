8059c5cb74a861458a8f41a31db4d121efede35f
PabloJavier22 <pmungar408@g.educaand.es>
2023-01-25 20:33:56 +0100

    quinto commit

---
+++ src/main/java/com/Pablo/Daniel/parkingmanagerdemo/User/UserController.java
@@ -0,0 +1,14 @@
+package com.Pablo.Daniel.parkingmanagerdemo.User;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class UserController {
+    private UserRepository repository = new UserRepository();
+
+    @GetMapping("/usuarios")
+    public String displayUsers(){
+        return "listaususarios";
+    }
+}

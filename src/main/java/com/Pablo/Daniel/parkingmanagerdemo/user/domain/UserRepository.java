dc697151e86fc3026528c1683a67b3e5a0e45da3
PabloJavier22 <pmungar408@g.educaand.es>
2023-02-22 20:30:30 +0100

    correccion 2

--- src/main/java/com/Pablo/Daniel/parkingmanagerdemo/user/domain/UserRepository.java
+++ src/main/java/com/Pablo/Daniel/parkingmanagerdemo/user/domain/UserRepository.java
@@ -3,8 +3,6 @@ package com.Pablo.Daniel.parkingmanagerdemo.user.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.Pablo.Daniel.parkingmanagerdemo.user.domain.User;
-
 @Repository
 public interface UserRepository extends CrudRepository<User,Long>{
 

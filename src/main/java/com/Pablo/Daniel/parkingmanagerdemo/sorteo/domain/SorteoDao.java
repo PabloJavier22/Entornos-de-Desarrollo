1c74bd732d4bb89b10b44be3f126763419a3151c
PabloJavier22 <pmungar408@g.educaand.es>
2023-03-22 20:35:39 +0100

    v.estable

---
+++ src/main/java/com/Pablo/Daniel/parkingmanagerdemo/sorteo/domain/SorteoDao.java
@@ -0,0 +1,39 @@
+package com.Pablo.Daniel.parkingmanagerdemo.sorteo.domain;
+
+public class SorteoDao {
+
+    private String descripcion;
+    private String fecha;
+    private String estado;  
+    
+
+    public SorteoDao(){
+        this("","","");
+    }
+
+    
+    public SorteoDao(String descripcion, String fecha, String estado) {
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.estado = estado;
+    }    
+    
+    public String getDescripcion() {
+        return descripcion;
+    }   
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}   
